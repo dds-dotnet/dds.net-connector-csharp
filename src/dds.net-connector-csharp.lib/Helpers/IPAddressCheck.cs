c70bc6914816587aa300bc6beecb03095aa75ef2
usama <mahmoodify@gmail.com>
2023-03-14 15:50:22 +0500

    adding ip address checker

---
+++ src/dds.net-connector-csharp.lib/Helpers/IPAddressCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DDS.Net.Connector.Helpers
+{
+    internal static class IPAddressCheck
+    {
+        private static Regex ipv4AddressPattern =
+            new(@"\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*");
+
+        public static bool IsValidIPv4Address(this string ipv4Address)
+        {
+            return ipv4AddressPattern.IsMatch(ipv4Address);
+        }
+    }
+}
