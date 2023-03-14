14cd8e91d8017c63fa33865fb71da0c067338e1e
usama <mahmoodify@gmail.com>
2023-03-14 06:07:41 +0500

    comments updt

--- src/dds.net-connector-csharp.lib/Types/PacketId.cs
+++ src/dds.net-connector-csharp.lib/Types/PacketId.cs
@@ -3,28 +3,28 @@
     internal enum PacketId
     {
         /// <summary>
-        /// Initialization information / configuration exchange
+        /// Initialization information / configuration exchange.
         /// </summary>
         HandShake = 0,
         /// <summary>
-        /// Registering variables with the server
+        /// Registering variables with the server.
         /// </summary>
         VariablesRegistration = 1,
         /// <summary>
-        /// Updating variable values at the server
+        /// Updating variable values at the server.
         /// </summary>
         VariablesUpdateAtServer = 2,
         /// <summary>
-        /// Updating variable values at the client
+        /// Updating variable values at the client.
         /// </summary>
         VariablesUpdateFromServer = 3,
         /// <summary>
-        /// Error responses from the server
+        /// Error responses from the server.
         /// </summary>
         ErrorResponseFromServer = 4,
 
         /// <summary>
-        /// Unknown packet
+        /// Unknown packet.
         /// </summary>
         UnknownPacket
     }
