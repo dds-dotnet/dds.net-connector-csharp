0f98bc9e9435bc9e606bf8458b534117dea4cd6e
usama <mahmoodify@gmail.com>
2023-03-14 05:26:22 +0500

    adding packetid

---
+++ src/dds.net-connector-csharp.lib/Types/PacketId.cs
@@ -0,0 +1,31 @@
+﻿namespace DDS.Net.Connector.Types
+{
+    internal enum PacketId
+    {
+        /// <summary>
+        /// Initialization information / configuration exchange
+        /// </summary>
+        HandShake = 0,
+        /// <summary>
+        /// Registering variables with the server
+        /// </summary>
+        VariablesRegistration = 1,
+        /// <summary>
+        /// Updating variable values at the server
+        /// </summary>
+        VariablesUpdateAtServer = 2,
+        /// <summary>
+        /// Updating variable values at the client
+        /// </summary>
+        VariablesUpdateFromServer = 3,
+        /// <summary>
+        /// Error responses from the server
+        /// </summary>
+        ErrorResponseFromServer = 4,
+
+        /// <summary>
+        /// Unknown packet
+        /// </summary>
+        UnknownPacket
+    }
+}
