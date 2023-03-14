f72be85c65ae4d510e15afffa553aba0cee000fd
usama <mahmoodify@gmail.com>
2023-03-14 15:38:38 +0500

    adding consts for info

---
+++ src/dds.net-connector-csharp.app.wpf/Configuration/AppConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDS.Net.Connector.WpfApp.Configuration
+{
+    internal static class AppConstants
+    {
+        static private readonly DateTime TIMESTAMP = DateTime.Now;
+        static private readonly string TIMESTAMP_TEXT =
+            $"{TIMESTAMP.Year}_{TIMESTAMP.Month,02:00}_{TIMESTAMP.Day,02:00}_" +
+            $"{TIMESTAMP.Hour,02:00}_{TIMESTAMP.Minute,02:00}_{TIMESTAMP.Second,02:00}";
+
+        static public readonly string LOG_FILENAME = $"Log/{TIMESTAMP_TEXT}.txt";
+        static public readonly string SERVER_CONFIG_FILENAME = "Configuration/server-conf.ini";
+        static public readonly string VARIABLES_CONFIG_FILENAME = "Configuration/variables-conf.ini";
+    }
+}
