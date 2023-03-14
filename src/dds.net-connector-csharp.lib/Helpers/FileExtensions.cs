4c2806e432b41f3b2d24c370e4e0b8fd3fc65f11
usama <mahmoodify@gmail.com>
2023-03-14 15:15:49 +0500

    adding file helper extensions

---
+++ src/dds.net-connector-csharp.lib/Helpers/FileExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace DDS.Net.Connector.Helpers
+{
+    public static class FileExtensions
+    {
+        internal static List<string> TrimmedParts(this string str, char splitter)
+        {
+            if (string.IsNullOrEmpty(str) == false)
+            {
+                string[] parts = str.Split(splitter, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length > 0)
+                {
+                    return new List<string>(parts);
+                }
+            }
+
+            return new List<string>();
+        }
+
+        public static void CreateFoldersForRelativeFilename(this string filename)
+        {
+            if (string.IsNullOrEmpty(filename)) return;
+
+            string[] folders = filename.Split(
+                new char[] { '\\', '/' },
+                StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            if (folders.Length > 1)
+            {
+                string foldername = "";
+
+                for (int i = 0; i < folders.Length - 1; i++)
+                {
+                    if (foldername == "")
+                    {
+                        foldername = folders[i];
+                    }
+                    else
+                    {
+                        foldername = $"{foldername}{Path.DirectorySeparatorChar}{folders[i]}";
+                    }
+                }
+
+                Directory.CreateDirectory(foldername);
+            }
+        }
+    }
+}
