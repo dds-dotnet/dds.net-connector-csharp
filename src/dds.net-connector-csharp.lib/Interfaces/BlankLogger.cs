b9964391870e6a4a4e8c9dca22bb23bbbcd01aeb
usama <mahmoodify@gmail.com>
2023-03-14 16:06:47 +0500

    adding blank logger

---
+++ src/dds.net-connector-csharp.lib/Interfaces/BlankLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace DDS.Net.Connector.Interfaces
+{
+    public class BlankLogger : ILogger
+    {
+        public void Error(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+    }
+}
