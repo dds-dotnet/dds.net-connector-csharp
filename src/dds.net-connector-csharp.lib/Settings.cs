f9e42437d7c62dc6bfb2a27b65cb13f79e70fe68
usama <mahmoodify@gmail.com>
2023-03-14 05:58:12 +0500

    adding settig for time slot

---
+++ src/dds.net-connector-csharp.lib/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace DDS.Net.Connector
+{
+    internal class Settings
+    {
+        /// <summary>
+        /// Time-slice for updating periodic variables to the server.
+        /// </summary>
+        internal static int BASE_TIME_SLOT_MS = 50;
+    }
+}
