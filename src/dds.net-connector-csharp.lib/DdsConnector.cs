7dfdd4b6e9516c0a6d618fd3e4674f9bda0ace5d
usama <mahmoodify@gmail.com>
2023-03-14 16:04:16 +0500

    adding logger iface

--- src/dds.net-connector-csharp.lib/DdsConnector.cs
+++ src/dds.net-connector-csharp.lib/DdsConnector.cs
@@ -1,4 +1,5 @@
 ﻿using DDS.Net.Connector.Helpers;
+using DDS.Net.Connector.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,16 @@ namespace DDS.Net.Connector
 {
     public class DdsConnector
     {
-        public DdsConnector(string serverIPv4, ushort serverPortTCP)
+        public DdsConnector(
+            string serverIPv4,
+            ushort serverPortTCP,
+            ILogger logger)
         {
             ServerAddressIPv4 = serverIPv4 ?? throw new ArgumentNullException(nameof(serverIPv4));
             ServerPortTCP = serverPortTCP;
 
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            
             if (serverIPv4.IsValidIPv4Address())
             {
                 ServerAddressIPv4 = ServerAddressIPv4.RemoveSpaces();
@@ -22,5 +28,6 @@ namespace DDS.Net.Connector
 
         public string ServerAddressIPv4 { get; }
         public ushort ServerPortTCP { get; }
+        private ILogger Logger { get; }
     }
 }
