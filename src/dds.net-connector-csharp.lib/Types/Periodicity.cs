bd8ddfc54a984cae0d37d9060cb8dac80c88543a
usama <mahmoodify@gmail.com>
2023-03-14 06:01:39 +0500

    adding periodicity consts

---
+++ src/dds.net-connector-csharp.lib/Types/Periodicity.cs
@@ -0,0 +1,30 @@
+﻿namespace DDS.Net.Connector.Types
+{
+    internal enum Periodicity
+    {
+        /// <summary>
+        /// Updates only when changed
+        /// </summary>
+        OnChange = 0,
+        /// <summary>
+        /// Updates every { <c cref="Settings.BASE_TIME_SLOT_MS">BASE_TIME_SLOT_MS * 1</c> } milliseconds
+        /// </summary>
+        Highest,
+        /// <summary>
+        /// Updates every { <c cref="Settings.BASE_TIME_SLOT_MS">BASE_TIME_SLOT_MS * 2</c> } milliseconds
+        /// </summary>
+        High,
+        /// <summary>
+        /// Updates every { <c cref="Settings.BASE_TIME_SLOT_MS">BASE_TIME_SLOT_MS * 4</c> } milliseconds
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// Updates every { <c cref="Settings.BASE_TIME_SLOT_MS">BASE_TIME_SLOT_MS * 8</c> } milliseconds
+        /// </summary>
+        Low,
+        /// <summary>
+        /// Updates every { <c cref="Settings.BASE_TIME_SLOT_MS">BASE_TIME_SLOT_MS * 16</c> } milliseconds
+        /// </summary>
+        Lowest
+    }
+}
