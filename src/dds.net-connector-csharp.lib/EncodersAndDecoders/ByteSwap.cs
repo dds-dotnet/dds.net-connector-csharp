d90c9494660e34ea98f6e7627c4df8cec67de3d5
usama <mahmoodify@gmail.com>
2023-03-14 05:48:39 +0500

    adding swapping of bytes - little/big endians

---
+++ src/dds.net-connector-csharp.lib/EncodersAndDecoders/ByteSwap.cs
@@ -0,0 +1,17 @@
+﻿namespace DDS.Net.Connector.EncodersAndDecoders
+{
+    internal static class ByteSwap
+    {
+        internal static byte[] SwapBytes(this byte[] data)
+        {
+            for (int i = 0; i < data.Length / 2; i++)
+            {
+                byte temp = data[i];
+                data[i] = data[data.Length - i - 1];
+                data[data.Length - i - 1] = temp;
+            }
+
+            return data;
+        }
+    }
+}
