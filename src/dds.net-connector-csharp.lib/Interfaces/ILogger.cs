463a2046d14bbe7b7c706350fec07a2858be5353
usama <mahmoodify@gmail.com>
2023-03-14 15:05:28 +0500

    adding logger iface

---
+++ src/dds.net-connector-csharp.lib/Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace DDS.Net.Connector.Interfaces
+{
+    public enum LogLevel
+    {
+        Information, Warning, Error
+    }
+
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
