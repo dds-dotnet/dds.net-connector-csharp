cbf37efebb38a7c4ce54bb6590da58156c1f89a2
usama <mahmoodify@gmail.com>
2023-03-14 05:31:20 +0500

    adding sanity check - data size

---
+++ src/dds.net-connector-csharp.lib/EncodersAndDecoders/SanityChecks.cs
@@ -0,0 +1,39 @@
+﻿namespace DDS.Net.Connector.EncodersAndDecoders
+{
+    internal static class SanityChecks
+    {
+        /// <summary>
+        /// Checks if the data buffer can be read / written with required number of bytes
+        /// </summary>
+        /// <param name="data">The data buffer</param>
+        /// <param name="offset">Offset in the buffer</param>
+        /// <param name="requiredSize">Number of bytes required in the buffer</param>
+        /// <returns>The same data buffer - for chaining</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static byte[] ThrowIfNotHavingRequiredBytes(this byte[] data, ref int offset, int requiredSize)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(offset),
+                    $"Offset is negative: {offset}");
+            }
+
+            if (offset + requiredSize - 1 >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(offset),
+                    $"Array of {data.Length} bytes requires to have " +
+                    $"data with {requiredSize} bytes starting at {offset} byte offset");
+            }
+
+            return data;
+        }
+    }
+}
