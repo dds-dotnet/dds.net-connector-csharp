6356fb4d635e0998ed363cfb594fe215f1d287e9
usama <mahmoodify@gmail.com>
2023-03-14 23:09:31 +0500

    adding version number

--- src/dds.net-connector-csharp.lib/Settings.cs
+++ src/dds.net-connector-csharp.lib/Settings.cs
@@ -3,6 +3,10 @@
     internal class Settings
     {
         /// <summary>
+        /// Version for the connector.
+        /// </summary>
+        internal static string CONNECTOR_VERSION = "0.0.0";
+        /// <summary>
         /// Time-slice for updating periodic variables to the server.
         /// </summary>
         internal static int BASE_TIME_SLOT_MS = 50;
