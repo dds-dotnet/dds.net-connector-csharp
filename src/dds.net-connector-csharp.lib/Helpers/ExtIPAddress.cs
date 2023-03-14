1775518e8b92f0fa9e093a9b5a5c51f5676573b6
usama <mahmoodify@gmail.com>
2023-03-14 20:41:04 +0500

    changing extension method visibility to internal

--- src/dds.net-connector-csharp.lib/Helpers/ExtIPAddress.cs
+++ src/dds.net-connector-csharp.lib/Helpers/ExtIPAddress.cs
@@ -7,7 +7,7 @@ namespace DDS.Net.Connector.Helpers
         private static Regex ipv4AddressPattern =
             new(@"\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*");
 
-        public static bool IsValidIPv4Address(this string ipv4Address)
+        internal static bool IsValidIPv4Address(this string ipv4Address)
         {
             return ipv4AddressPattern.IsMatch(ipv4Address);
         }
