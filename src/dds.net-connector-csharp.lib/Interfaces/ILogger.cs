c42d0e232320b504ede73caa7a323d0c3aef6e53
usama <mahmoodify@gmail.com>
2023-03-14 15:11:41 +0500

    adding info comments

--- src/dds.net-connector-csharp.lib/Interfaces/ILogger.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/ILogger.cs
@@ -1,14 +1,43 @@
 ﻿namespace DDS.Net.Connector.Interfaces
 {
+    /// <summary>
+    /// Level of log messages.
+    /// </summary>
     public enum LogLevel
     {
-        Information, Warning, Error
+        /// <summary>
+        /// Least important information.
+        /// </summary>
+        Information,
+        /// <summary>
+        /// Medium-level of information.
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// Critical-level of information.
+        /// </summary>
+        Error
     }
 
+    /// <summary>
+    /// Interface for outputting log messages.
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Writes <c cref="LogLevel.Information">Information</c>-level messages to log.
+        /// </summary>
+        /// <param name="message">The message.</param>
         void Info(string message);
+        /// <summary>
+        /// Writes <c cref="LogLevel.Warning">Warning</c>-level messages to log.
+        /// </summary>
+        /// <param name="message">The message.</param>
         void Warning(string message);
+        /// <summary>
+        /// Writes <c cref="LogLevel.Error">Error</c>-level messages to log.
+        /// </summary>
+        /// <param name="message">The message.</param>
         void Error(string message);
     }
 }
