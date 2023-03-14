d243ed1ba82f1953cb01be47d1fbc3834a7d7e0c
usama <mahmoodify@gmail.com>
2023-03-14 16:15:56 +0500

    adding info

--- src/dds.net-connector-csharp.lib/Interfaces/BlankLogger.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/BlankLogger.cs
@@ -1,5 +1,9 @@
 ﻿namespace DDS.Net.Connector.Interfaces
 {
+    /// <summary>
+    /// Class <c>BlankLogger</c> implements <c>ILogger</c>, though does nothing
+    /// with the provided messages. Use it to discard all the messages.
+    /// </summary>
     public class BlankLogger : ILogger
     {
         public void Error(string message)
