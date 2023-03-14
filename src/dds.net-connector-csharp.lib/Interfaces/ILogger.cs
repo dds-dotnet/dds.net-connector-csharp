5b14c4bc935e73c5ef8be7c3c037060c59e4f642
usama <mahmoodify@gmail.com>
2023-03-14 20:38:11 +0500

    updating info

--- src/dds.net-connector-csharp.lib/Interfaces/ILogger.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/ILogger.cs
@@ -25,17 +25,17 @@
     public interface ILogger
     {
         /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level messages to log.
+        /// Writes <c cref="LogLevel.Information">Information</c>-level message to log.
         /// </summary>
         /// <param name="message">The message.</param>
         void Info(string message);
         /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level messages to log.
+        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message to log.
         /// </summary>
         /// <param name="message">The message.</param>
         void Warning(string message);
         /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level messages to log.
+        /// Writes <c cref="LogLevel.Error">Error</c>-level message to log.
         /// </summary>
         /// <param name="message">The message.</param>
         void Error(string message);
