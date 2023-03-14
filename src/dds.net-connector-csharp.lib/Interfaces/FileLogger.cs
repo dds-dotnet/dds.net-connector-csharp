ffddfc9150865d1d195b20a6d66c51f48e070902
usama <mahmoodify@gmail.com>
2023-03-14 20:36:17 +0500

    adding file logger

---
+++ src/dds.net-connector-csharp.lib/Interfaces/FileLogger.cs
@@ -0,0 +1,85 @@
+﻿using DDS.Net.Connector.Helpers;
+
+namespace DDS.Net.Connector.Interfaces
+{
+    internal class FileLogger : ILogger, IDisposable
+    {
+        private readonly LogLevel _logLevel;
+        private StreamWriter? _writer;
+
+        public FileLogger(string filename, LogLevel logLevel = LogLevel.Information)
+        {
+            try
+            {
+                filename.CreateFoldersForRelativeFilename();
+
+                _writer = File.AppendText(filename);
+
+                _writer.WriteLine($"╔═══════════════════════════════════════════════════════════════════╗");
+                _writer.WriteLine($"║ DDS.Net Connector                                                 ║");
+                _writer.WriteLine($"║-------------------------------------------------------------------║");
+                _writer.WriteLine($"║ Starting@                                                         ║");
+                _writer.WriteLine($"║     Local time: {DateTime.Now,-35}               ║");
+                _writer.WriteLine($"║     UTC time:   {DateTime.UtcNow,-35}               ║");
+                _writer.WriteLine($"╚═══════════════════════════════════════════════════════════════════╝");
+
+                _writer.Flush();
+                _writer.AutoFlush = true;
+            }
+            catch (Exception ex)
+            {
+                _writer = null;
+                throw new Exception($"Cannot write log file \"{filename}\" - {ex.Message}");
+            }
+
+            _logLevel = logLevel;
+        }
+
+        public void Dispose()
+        {
+            if (_writer != null)
+            {
+                _writer.WriteLine($"╔═══════════════════════════════════════════════════════════════════╗");
+                _writer.WriteLine($"║ Stopping@                                                         ║");
+                _writer.WriteLine($"║     Local time: {DateTime.Now,-35}               ║");
+                _writer.WriteLine($"║     UTC time:   {DateTime.UtcNow,-35}               ║");
+                _writer.WriteLine($"╚═══════════════════════════════════════════════════════════════════╝");
+                _writer.WriteLine($"");
+
+                _writer.Flush();
+                _writer.Dispose();
+                _writer = null;
+            }
+        }
+
+        public void Error(string message)
+        {
+            lock (this)
+            {
+                _writer?.WriteLine($"Error: {message}");
+            }
+        }
+
+        public void Info(string message)
+        {
+            if (_logLevel == LogLevel.Information)
+            {
+                lock (this)
+                {
+                    _writer?.WriteLine(message);
+                }
+            }
+        }
+
+        public void Warning(string message)
+        {
+            if (_logLevel != LogLevel.Error)
+            {
+                lock (this)
+                {
+                    _writer?.WriteLine($"Warning: {message}");
+                }
+            }
+        }
+    }
+}
