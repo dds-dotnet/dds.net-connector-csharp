d39c5692a56f4b1384e140baf1a412f6842ed792
usama <mahmoodify@gmail.com>
2023-03-14 06:11:41 +0500

    adding variable type

---
+++ src/dds.net-connector-csharp.lib/Types/VariableType.cs
@@ -0,0 +1,20 @@
+﻿namespace DDS.Net.Connector.Types
+{
+    public enum VariableType
+    {
+        /// <summary>
+        /// Represents the very basic variable type,
+        /// e.g., string, integer, float, etc.
+        /// </summary>
+        Primitive,
+        /// <summary>
+        /// Represents a sequence of bytes (unsigned bytes).
+        /// </summary>
+        RawBytes,
+
+        /// <summary>
+        /// Unknown type.
+        /// </summary>
+        UnknownVariableType
+    }
+}
