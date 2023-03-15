eb1390f0f18ce39db5964eb5c5876aa4301cae38
usama <mahmoodify@gmail.com>
2023-03-16 01:56:01 +0500

    passing in generic data

--- src/dds.net-connector-csharp.lib/Helpers/EasyThread.cs
+++ src/dds.net-connector-csharp.lib/Helpers/EasyThread.cs
@@ -9,28 +9,32 @@ namespace DDS.Net.Connector.Helpers
     /// <summary>
     /// Class <c>EasyThread</c> helps in management of thread.
     /// </summary>
-    internal class EasyThread
+    internal class EasyThread<T>
     {
-        private readonly bool isPeriodic;                   // True means to run the action at intervals
+        private readonly bool isPeriodic;                      // True means to run the action at intervals
 
-        private readonly Func<bool> threadFunction = null!; // Function to be invoked continuously
-        private readonly int sleepTimeWhenDoneNothing;      // Sleep for milliseconds when the function did nothing
+        private readonly T data;                               // Data to be passed-in to the function
 
-        private readonly Action periodicFunction = null!;   // Function to be invoked periodically
-        private readonly int timeInterval;                  // Periodic invocation time interval
+        private readonly Func<T, bool> threadFunction = null!; // Function to be invoked continuously
+        private readonly int sleepTimeWhenDoneNothing;         // Sleep for milliseconds when the function did nothing
+
+        private readonly Action<T> periodicFunction = null!;   // Function to be invoked periodically
+        private readonly int timeInterval;                     // Periodic invocation time interval
 
         /// <summary>
         /// Initializes the instance with continuously invoked thread function.
         /// </summary>
         /// <param name="threadFunction">Target function - returns [True/False] if work is done during the iteration.</param>
+        /// <param name="data">Data to be passed to the function.</param>
         /// <param name="sleepTimeWhenDoneNothing">Sleep time (milliseconds) when the thread function did nothing.</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public EasyThread(Func<bool> threadFunction, int sleepTimeWhenDoneNothing = 10)
+        public EasyThread(Func<T, bool> threadFunction, T data, int sleepTimeWhenDoneNothing = 10)
         {
             isPeriodic = false;
 
             this.threadFunction = threadFunction ?? throw new ArgumentNullException(nameof(threadFunction));
+            this.data = data;
             this.sleepTimeWhenDoneNothing = sleepTimeWhenDoneNothing;
 
             if (sleepTimeWhenDoneNothing <= 0)
@@ -42,14 +46,16 @@ namespace DDS.Net.Connector.Helpers
         /// Initializes the instance with periodically invoked thread function.
         /// </summary>
         /// <param name="periodicFunction">Periodically invoked function.</param>
+        /// <param name="data">Data to be passed to the function.</param>
         /// <param name="timeInterval">Time period (milliseconds) between invokations - must be greater than zero.</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public EasyThread(Action periodicFunction, int timeInterval)
+        public EasyThread(Action<T> periodicFunction, T data, int timeInterval)
         {
             isPeriodic = true;
 
             this.periodicFunction = periodicFunction ?? throw new ArgumentNullException(nameof(periodicFunction));
+            this.data = data;
             this.timeInterval = timeInterval;
 
             if (timeInterval < 0)
@@ -118,7 +124,7 @@ namespace DDS.Net.Connector.Helpers
         {
             timer.Change(Timeout.Infinite, Timeout.Infinite);
 
-            periodicFunction.Invoke();
+            periodicFunction.Invoke(data);
 
             lock (this)
             {
@@ -142,7 +148,7 @@ namespace DDS.Net.Connector.Helpers
         {
             while (isRunning)
             {
-                bool doneAnything = threadFunction.Invoke();
+                bool doneAnything = threadFunction.Invoke(data);
 
                 if (!doneAnything)
                 {
