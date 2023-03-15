d114eb7c48754a07532474eee197b22f7fbe4c04
usama <mahmoodify@gmail.com>
2023-03-16 01:24:02 +0500

    outlining the thread control

--- src/dds.net-connector-csharp.lib/Helpers/EasyThread.cs
+++ src/dds.net-connector-csharp.lib/Helpers/EasyThread.cs
@@ -11,27 +11,65 @@ namespace DDS.Net.Connector.Helpers
     /// </summary>
     internal class EasyThread
     {
-        private readonly bool isPeriodicCall;   // True means to run the action at intervals
-        private readonly Action threadFunction; // Function to be invoked
-        private readonly int timeInterval;
+        private readonly bool isPeriodic;                   // True means to run the action at intervals
+        private readonly Func<bool> threadFunction = null!; // Function to be invoked continuously
 
-        public EasyThread(Action threadFunction)
+        private readonly Action periodicFunction = null!;   // Function to be invoked periodically
+        private readonly int timeInterval;                  // Periodic invocation time interval
+
+        /// <summary>
+        /// Initializes the instance with continuously invoked thread function.
+        /// </summary>
+        /// <param name="threadFunction">Target function - returns [True/False] if work is done during the iteration.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public EasyThread(Func<bool> threadFunction)
         {
+            isPeriodic = false;
+
             this.threadFunction = threadFunction ?? throw new ArgumentNullException(nameof(threadFunction));
-            isPeriodicCall = false;
         }
-
-        public EasyThread(Action threadFunction, int timeInterval)
+        /// <summary>
+        /// Initializes the instance with periodically invoked thread function.
+        /// </summary>
+        /// <param name="periodicFunction">Periodically invoked function.</param>
+        /// <param name="timeInterval">Time period (milliseconds) between invokations - must be greater than zero.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public EasyThread(Action periodicFunction, int timeInterval)
         {
-            this.threadFunction = threadFunction ?? throw new ArgumentNullException(nameof(threadFunction));
+            isPeriodic = true;
+
+            this.periodicFunction = periodicFunction ?? throw new ArgumentNullException(nameof(periodicFunction));
             this.timeInterval = timeInterval;
 
             if (timeInterval < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(timeInterval));
             }
+        }
 
-            isPeriodicCall = true;
+        private volatile bool isRunning = false;
+
+        public void Start()
+        {
+            lock (this)
+            {
+                if (!isRunning)
+                {
+                    isRunning = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (this)
+            {
+                if (!isRunning)
+                {
+                    isRunning = false;
+                }
+            }
         }
     }
 }
