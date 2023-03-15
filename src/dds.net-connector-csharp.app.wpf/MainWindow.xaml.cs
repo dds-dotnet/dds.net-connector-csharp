647db874d529360b3df44ea290ea118341709d0b
usama <mahmoodify@gmail.com>
2023-03-15 19:01:22 +0500

    removing default comments

--- src/dds.net-connector-csharp.app.wpf/MainWindow.xaml.cs
+++ src/dds.net-connector-csharp.app.wpf/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace DDS.Net.Connector.WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
