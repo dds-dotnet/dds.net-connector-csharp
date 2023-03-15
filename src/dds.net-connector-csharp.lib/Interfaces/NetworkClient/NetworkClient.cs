9b52949707ac0fbbecc1fc9fedcf6ab35416a84b
usama <mahmoodify@gmail.com>
2023-03-16 00:07:42 +0500

    adding class with blank impl

---
+++ src/dds.net-connector-csharp.lib/Interfaces/NetworkClient/NetworkClient.cs
@@ -0,0 +1,48 @@
+﻿using DDS.Net.Connector.Interfaces.SyncQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Connector.Interfaces.NetworkClient
+{
+    /// <summary>
+    /// Class <c>NetworkClient</c> implements <c>IThreadedNetworkClient</c> interface,
+    /// and provides connectivity with the server.
+    /// </summary>
+    internal class NetworkClient : IThreadedNetworkClient
+    {
+        private SyncQueue<PacketToServer> dataToServerQueue;
+        private SyncQueue<PacketFromServer> dataFromServerQueue;
+
+        /// <summary>
+        /// Initializes the client.
+        /// </summary>
+        public NetworkClient()
+        {
+            dataToServerQueue = new(1000);
+            dataFromServerQueue = new(1000);
+        }
+
+        public void Connect(string serverIPv4, ushort portTCP)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncQueueReader<PacketFromServer> GetDataQueueFromServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncQueueWriter<PacketToServer> GetDataQueueToServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
