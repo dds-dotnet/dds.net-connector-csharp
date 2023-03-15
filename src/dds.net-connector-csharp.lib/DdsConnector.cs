8cc4e5861ec716995641799141b7ad4634a1e04f
usama <mahmoodify@gmail.com>
2023-03-15 21:10:04 +0500

    adding doc comments for providers and consumers

--- src/dds.net-connector-csharp.lib/DdsConnector.cs
+++ src/dds.net-connector-csharp.lib/DdsConnector.cs
@@ -79,118 +79,293 @@ namespace DDS.Net.Connector
         /*                                                                                 */
         /***********************************************************************************/
 
+        //- 
+        //- Providers
+        //- 
+
+        /// <summary>
+        /// Registers a provider delegate for providing "String" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterStringProvider(string variableName, StringProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Boolean" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterBooleanProvider(string variableName, BooleanProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Signed Byte" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterByteProvider(string variableName, ByteProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Signed Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterWordProvider(string variableName, WordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Signed Double Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterDWordProvider(string variableName, DWordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Signed Quad Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterQWordProvider(string variableName, QWordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Unsigned Byte" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedByteProvider(string variableName, UnsignedByteProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Unsigned Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedWordProvider(string variableName, UnsignedWordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Unsigned Double Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedDWordProvider(string variableName, UnsignedDWordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Unsigned Quad Word" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedQWordProvider(string variableName, UnsignedQWordProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Single - 4 byte floating-point" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterSingleProvider(string variableName, SingleProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "Double - 8 byte floating-point" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterDoubleProvider(string variableName, DoubleProvider provider, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a provider delegate for providing "bytes array" to the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterRawBytesProvider(string variableName, RawBytesProvider provider, Periodicity periodicity)
         {
 
         }
 
+        /// <summary>
+        /// Unregisters providing named variable to the server.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
         public void UnregisterProvider(string variableName)
         {
 
         }
 
+
+        //- 
+        //- Consumers
+        //- 
+
+        /// <summary>
+        /// Registers a consumer delegate for receiving "String" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterStringConsumer(string variableName, StringConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Boolean" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterBooleanConsumer(string variableName, BooleanConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Signed Byte" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterByteConsumer(string variableName, ByteConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Signed Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterWordConsumer(string variableName, WordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Signed Double Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterDWordConsumer(string variableName, DWordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Signed Quad Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterQWordConsumer(string variableName, QWordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Unsigned Byte" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedByteConsumer(string variableName, UnsignedByteConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Unsigned Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedWordConsumer(string variableName, UnsignedWordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Unsigned Double Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedDWordConsumer(string variableName, UnsignedDWordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Unsigned Quad Word" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterUnsignedQWordConsumer(string variableName, UnsignedQWordConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Single - 4 byte floating-point number"
+        /// from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterSingleConsumer(string variableName, SingleConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "Double - 8 byte floating-point number"
+        /// from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterDoubleConsumer(string variableName, DoubleConsumer consumer, Periodicity periodicity)
         {
 
         }
+        /// <summary>
+        /// Registers a consumer delegate for receiving "bytes array" from the server at given periodicity.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="provider">The delegate.</param>
+        /// <param name="periodicity">Periodicity.</param>
         public void RegisterRawBytesConsumer(string variableName, RawBytesConsumer consumer, Periodicity periodicity)
         {
 
         }
 
-        public void RegisterConsumer(string variableName)
+        /// <summary>
+        /// Unregisters getting named variable from the server.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        public void UnregisterConsumer(string variableName)
         {
 
         }
