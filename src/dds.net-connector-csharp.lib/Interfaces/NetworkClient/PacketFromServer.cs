50860cdac6ae842de2d57d772369eee1377c37e1
usama <mahmoodify@gmail.com>
2023-03-16 00:39:25 +0500

    adding data prop

--- src/dds.net-connector-csharp.lib/Interfaces/NetworkClient/PacketFromServer.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/NetworkClient/PacketFromServer.cs
@@ -2,5 +2,11 @@
 {
     internal class PacketFromServer
     {
+        public byte[] Data { get; }
+
+        public PacketFromServer(byte[] data)
+        {
+            Data = data;
+        }
     }
 }
