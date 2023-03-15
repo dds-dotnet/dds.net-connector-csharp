1cca7845162f65f0b48e23df2af768f7c43796c1
usama <mahmoodify@gmail.com>
2023-03-15 20:05:46 +0500

    adding boolean delegate

--- src/dds.net-connector-csharp.lib/Delegates.cs
+++ src/dds.net-connector-csharp.lib/Delegates.cs
@@ -1,5 +1,8 @@
 ﻿namespace DDS.Net.Connector
 {
     public delegate string StringProvider(string variableName);
+    public delegate bool BooleanProvider(string variableName);
+
     public delegate void StringConsumer(string variableName, string variableValue);
+    public delegate void BooleanConsumer(string variableName, bool variableValue);
 }
