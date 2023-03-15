e1d4ce66fc89f21becf55376ca6b651972b5648b
usama <mahmoodify@gmail.com>
2023-03-15 23:24:59 +0500

    adding doc comments

--- src/dds.net-connector-csharp.lib/Interfaces/DefaultLogger/FileLogger.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/DefaultLogger/FileLogger.cs
@@ -2,11 +2,21 @@
 
 namespace DDS.Net.Connector.Interfaces.DefaultLogger
 {
+    /// <summary>
+    /// Class <c>FileLogger</c> implements <c>ILogger</c> interface, and dumps
+    /// log messages into a file.
+    /// </summary>
     public class FileLogger : ILogger, IDisposable
     {
         private readonly LogLevel _logLevel;
         private StreamWriter? _writer;
 
+        /// <summary>
+        /// Initializes class <c>FileLogger</c> to dump log messages into given file name.
+        /// </summary>
+        /// <param name="filename">Log file name.</param>
+        /// <param name="logLevel">Minimum log level.</param>
+        /// <exception cref="Exception"></exception>
         public FileLogger(string filename, LogLevel logLevel = LogLevel.Information)
         {
             try
