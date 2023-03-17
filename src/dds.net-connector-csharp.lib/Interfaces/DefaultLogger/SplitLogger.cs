da9aa890596426da85425e424e1b45bac6903562
usama <mahmoodify@gmail.com>
2023-03-17 19:05:51 +0500

    updating to make the logger disposable and allowing two to any number of loggers

--- src/dds.net-connector-csharp.lib/Interfaces/DefaultLogger/SplitLogger.cs
+++ src/dds.net-connector-csharp.lib/Interfaces/DefaultLogger/SplitLogger.cs
@@ -5,63 +5,84 @@
     /// provides a means of splitting log messages to multiple implementations
     /// of <c>ILogger</c> interface.
     /// </summary>
-    public class SplitLogger : ILogger
+    public class SplitLogger : ILogger, IDisposable
     {
-        private readonly ILogger logger01;
-        private readonly ILogger logger02;
-        private readonly ILogger logger03;
-        private readonly ILogger logger04;
-        private readonly ILogger logger05;
+        private readonly ILogger firstLogger;
+        private readonly ILogger secondLogger;
+        private readonly ILogger[]? loggers;
 
         /// <summary>
-        /// Initializes class <c>SplitLogger</c> for splitting logging messages
-        /// in two to five <c>ILogger</c> interface implementations.
+        /// Initializes class <c>SplitLogger</c> for splitting log messages
+        /// in at least two <c>ILogger</c> interface implementations.
         /// </summary>
-        /// <param name="logger01">Required first <c>ILogger</c> interface implementation.</param>
-        /// <param name="logger02">Required second <c>ILogger</c> interface implementation.</param>
-        /// <param name="logger03">Optional third <c>ILogger</c> interface implementation.</param>
-        /// <param name="logger04">Optional fourth <c>ILogger</c> interface implementation.</param>
-        /// <param name="logger05">Optional fifth <c>ILogger</c> interface implementation.</param>
+        /// <param name="firstLogger">Required first <c>ILogger</c> interface implementation.</param>
+        /// <param name="secondLogger">Required second <c>ILogger</c> interface implementation.</param>
+        /// <param name="loggers"><c>ILogger</c> interface implementations.</param>
         /// <exception cref="ArgumentNullException"></exception>
-        public SplitLogger(
-            ILogger logger01,
-            ILogger logger02,
-            ILogger logger03 = null!,
-            ILogger logger04 = null!,
-            ILogger logger05 = null!)
+        public SplitLogger(ILogger firstLogger, ILogger secondLogger, params ILogger[] loggers)
         {
-            this.logger01 = logger01 ?? throw new ArgumentNullException(nameof(logger01));
-            this.logger02 = logger02 ?? throw new ArgumentNullException(nameof(logger02));
-            this.logger03 = logger03;
-            this.logger04 = logger04;
-            this.logger05 = logger05;
+            this.firstLogger = firstLogger ?? throw new ArgumentNullException(nameof(firstLogger));
+            this.secondLogger = secondLogger ?? throw new ArgumentNullException(nameof(secondLogger));
+            this.loggers = loggers;
+        }
+
+        public void Dispose()
+        {
+            if (firstLogger is IDisposable firstDisposable) { firstDisposable.Dispose(); }
+            if (secondLogger is IDisposable secondDisposable) { secondDisposable.Dispose(); }
+
+            if (loggers != null && loggers.Length > 0)
+            {
+                foreach (var logger in loggers)
+                {
+                    if (logger != null && logger is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+            }
         }
 
         public void Error(string message)
         {
-            logger01?.Error(message);
-            logger02?.Error(message);
-            logger03?.Error(message);
-            logger04?.Error(message);
-            logger05?.Error(message);
+            firstLogger.Error(message);
+            secondLogger.Error(message);
+
+            if (loggers != null && loggers.Length > 0)
+            {
+                foreach (var logger in loggers)
+                {
+                    logger?.Error(message);
+                }
+            }
         }
 
         public void Info(string message)
         {
-            logger01?.Info(message);
-            logger02?.Info(message);
-            logger03?.Info(message);
-            logger04?.Info(message);
-            logger05?.Info(message);
+            firstLogger?.Info(message);
+            secondLogger?.Info(message);
+
+            if (loggers != null && loggers.Length > 0)
+            {
+                foreach (var logger in loggers)
+                {
+                    logger?.Info(message);
+                }
+            }
         }
 
         public void Warning(string message)
         {
-            logger01?.Warning(message);
-            logger02?.Warning(message);
-            logger03?.Warning(message);
-            logger04?.Warning(message);
-            logger05?.Warning(message);
+            firstLogger?.Warning(message);
+            secondLogger?.Warning(message);
+
+            if (loggers != null && loggers.Length > 0)
+            {
+                foreach (var logger in loggers)
+                {
+                    logger?.Warning(message);
+                }
+            }
         }
     }
 }
