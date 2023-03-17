c228d982aab778dabb317a0a0b8b866ce88cf2c6
usama <mahmoodify@gmail.com>
2023-03-17 16:41:08 +0500

    adding simple text block logger

---
+++ src/dds.net-connector-csharp.app.wpf/InterfaceImpl/TextBlockLogger.cs
@@ -0,0 +1,56 @@
+﻿using DDS.Net.Connector.Interfaces;
+using System.Windows.Controls;
+
+namespace DDS.Net.Connector.WpfApp.InterfaceImpl
+{
+    internal class TextBlockLogger : ILogger
+    {
+        public TextBlockLogger(TextBlock textBlock, LogLevel logLevel)
+        {
+            TextBlock = textBlock;
+            LogLevel = logLevel;
+        }
+
+        public TextBlock TextBlock { get; }
+        public LogLevel LogLevel { get; }
+
+        public void Error(string message)
+        {
+            lock (this)
+            {
+                TextBlock.Dispatcher.Invoke(() =>
+                {
+                    TextBlock.Text += $"Error: {message}\n";
+                });
+            }
+        }
+
+        public void Info(string message)
+        {
+            if (LogLevel == LogLevel.Information)
+            {
+                lock (this)
+                {
+                    TextBlock.Dispatcher.Invoke(() =>
+                    {
+                        TextBlock.Text += $"{message}\n";
+                    });
+                }
+            }
+        }
+
+        public void Warning(string message)
+        {
+            if (LogLevel != LogLevel.Error)
+            {
+                lock (this)
+                {
+                    TextBlock.Dispatcher.Invoke(() =>
+                    {
+                        TextBlock.Text += $"Warning: {message}\n";
+                    });
+                }
+            }
+        }
+    }
+}
