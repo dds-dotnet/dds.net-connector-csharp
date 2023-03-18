a01059800a240a00b75702d5a46ecff136f6366e
usama <mahmoodify@gmail.com>
2023-03-18 20:12:48 +0500

    adding doc comments and updating for baseclass changes

--- src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedQWordVariable.cs
+++ src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedQWordVariable.cs
@@ -3,13 +3,16 @@ using DDS.Net.Connector.Types.Enumerations;
 
 namespace DDS.Net.Connector.Types.Variables.Primitives
 {
+    /// <summary>
+    /// Class <c>UnsignedQWordVariable</c> is a sub-class of <c>BasePrimitive</c> and
+    /// represents <c cref="PrimitiveType.UnsignedQWord">PrimitiveType.UnsignedQWord</c> handling.
+    /// </summary>
     internal class UnsignedQWordVariable : BasePrimitive
     {
         public ulong Value { get; set; }
 
-        public UnsignedQWordVariable(ushort id, string name) : base(id, name)
+        public UnsignedQWordVariable(string name) : base(name, PrimitiveType.UnsignedQWord)
         {
-            PrimitiveType = PrimitiveType.UnsignedQWord;
         }
 
         public override int GetValueSizeOnBuffer()
