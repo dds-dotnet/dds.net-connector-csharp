5b4a3d7accd28f20fd2a840fffab9ec016550d70
usama <mahmoodify@gmail.com>
2023-03-18 20:07:41 +0500

    adding doc comments and updating for baseclass

--- src/dds.net-connector-csharp.lib/Types/Variables/Primitives/QWordVariable.cs
+++ src/dds.net-connector-csharp.lib/Types/Variables/Primitives/QWordVariable.cs
@@ -3,13 +3,16 @@ using DDS.Net.Connector.Types.Enumerations;
 
 namespace DDS.Net.Connector.Types.Variables.Primitives
 {
+    /// <summary>
+    /// Class <c>QWordVariable</c> is a sub-class of <c>BasePrimitive</c> and
+    /// represents <c cref="PrimitiveType.DWord">PrimitiveType.DWord</c> handling.
+    /// </summary>
     internal class QWordVariable : BasePrimitive
     {
         public long Value { get; set; }
 
-        public QWordVariable(ushort id, string name) : base(id, name)
+        public QWordVariable(string name) : base(name, PrimitiveType.QWord)
         {
-            PrimitiveType = PrimitiveType.QWord;
         }
 
         public override int GetValueSizeOnBuffer()
