83e3bd0cc18d7f8a94aa51a6338cd712c2f68b55
usama <mahmoodify@gmail.com>
2023-03-19 00:37:27 +0500

    adding value refresh +  value provider & subscribers

--- src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedDWordVariable.cs
+++ src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedDWordVariable.cs
@@ -11,9 +11,17 @@ namespace DDS.Net.Connector.Types.Variables.Primitives
     {
         public uint Value { get; set; }
 
-        public UnsignedDWordVariable(string name, Periodicity periodicity)
-            : base(name,  PrimitiveType.UnsignedDWord, periodicity)
+        public UnsignedDWordProvider? ValueProvider { get; private set; }
+        public event UnsignedDWordConsumer? ValueConsumer;
+
+        public UnsignedDWordVariable(
+                    string name,
+                    Periodicity periodicity,
+                    UnsignedDWordProvider unsignedDWordProvider)
+
+            : base(name, PrimitiveType.UnsignedDWord, periodicity)
         {
+            ValueProvider = unsignedDWordProvider;
         }
 
         public override int GetValueSizeOnBuffer()
@@ -24,6 +32,22 @@ namespace DDS.Net.Connector.Types.Variables.Primitives
         public override void WriteValueOnBuffer(ref byte[] buffer, ref int offset)
         {
             buffer.WriteUnsignedDWord(ref offset, Value);
+        }
+
+        public override bool RefreshValue()
+        {
+            if (ValueProvider != null)
+            {
+                uint newValue = ValueProvider(Name);
+
+                if (Value != newValue)
+                {
+                    Value = newValue;
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
