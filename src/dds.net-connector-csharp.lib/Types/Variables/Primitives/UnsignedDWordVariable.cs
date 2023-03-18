4821991374e195b6c5d36bf3c720052ccd91e998
usama <mahmoodify@gmail.com>
2023-03-18 20:11:47 +0500

    adding doc comments and updating for baseclass updates

--- src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedDWordVariable.cs
+++ src/dds.net-connector-csharp.lib/Types/Variables/Primitives/UnsignedDWordVariable.cs
@@ -3,13 +3,16 @@ using DDS.Net.Connector.Types.Enumerations;
 
 namespace DDS.Net.Connector.Types.Variables.Primitives
 {
+    /// <summary>
+    /// Class <c>UnsignedDWordVariable</c> is a sub-class of <c>BasePrimitive</c> and
+    /// represents <c cref="PrimitiveType.UnsignedDWord">PrimitiveType.UnsignedDWord</c> handling.
+    /// </summary>
     internal class UnsignedDWordVariable : BasePrimitive
     {
         public uint Value { get; set; }
 
-        public UnsignedDWordVariable(ushort id, string name) : base(id, name)
+        public UnsignedDWordVariable(string name) : base(name,  PrimitiveType.UnsignedDWord)
         {
-            PrimitiveType = PrimitiveType.UnsignedDWord;
         }
 
         public override int GetValueSizeOnBuffer()
