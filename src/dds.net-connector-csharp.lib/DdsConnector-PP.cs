d9f37660d6a6943e939a462e36bf581f5ff02c78
usama <mahmoodify@gmail.com>
2023-03-19 16:16:37 +0500

    adding simple reg/unreg

--- src/dds.net-connector-csharp.lib/DdsConnector-PP.cs
+++ src/dds.net-connector-csharp.lib/DdsConnector-PP.cs
@@ -1,5 +1,6 @@
 ﻿using DDS.Net.Connector.EncodersAndDecoders;
 using DDS.Net.Connector.Types.Enumerations;
+using DDS.Net.Connector.Types.Variables;
 
 namespace DDS.Net.Connector
 {
@@ -51,7 +52,63 @@ namespace DDS.Net.Connector
 
         private void ParseVariablesRegistration(ref byte[] data, ref int offset)
         {
-            throw new NotImplementedException();
+            lock (variablesMutex)
+            {
+                while (offset < data.Length)
+                {
+                    (string variableName, ushort variableId, bool isRegister) =
+                                    ReadVariableRegistrationElements(ref data, ref offset);
+
+                    if (isRegister)
+                    {
+                        if (downloadVariablesToBeRegistered.ContainsKey(variableName))
+                        {
+                            BaseVariable theVar = downloadVariablesToBeRegistered[variableName];
+                            theVar.AssignId(variableId);
+
+                            downloadVariables.Add(variableName, theVar);
+                            downloadVariablesToBeRegistered.Remove(variableName);
+                        }
+                        else if (uploadVariablesToBeRegistered.ContainsKey(variableName))
+                        {
+                            BaseVariable theVar = uploadVariablesToBeRegistered[variableName];
+                            theVar.AssignId(variableId);
+
+                            uploadVariables.Add(variableName, theVar);
+                            uploadVariablesToBeRegistered.Remove(variableName);
+                        }
+                    }
+                    else
+                    {
+                        if (downloadVariables.ContainsKey(variableName))
+                        {
+                            BaseVariable theVar = downloadVariables[variableName];
+                            theVar.Reset();
+
+                            downloadVariablesToBeRegistered.Add(variableName, theVar);
+                            downloadVariables.Remove(variableName);
+                        }
+                        else if (uploadVariables.ContainsKey(variableName))
+                        {
+                            BaseVariable theVar = uploadVariables[variableName];
+                            theVar.Reset();
+
+                            uploadVariablesToBeRegistered.Add(variableName, theVar);
+                            uploadVariables.Remove(variableName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static (string variableName, ushort variableId, bool isRegister)
+            ReadVariableRegistrationElements(ref byte[] data, ref int offset)
+        {
+            string variableName = data.ReadString(ref offset);
+            ushort variableId = data.ReadUnsignedWord(ref offset);
+            bool isRegister = data.ReadBoolean(ref offset);
+
+            return (variableName, variableId, isRegister);
         }
 
         private void ParseVariablesUpdateFromServer(ref byte[] data, ref int offset)
