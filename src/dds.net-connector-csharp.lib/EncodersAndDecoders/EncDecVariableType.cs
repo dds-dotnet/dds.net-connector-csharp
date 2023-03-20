5ef314c978927da2689426fce738e428728e3224
usama <mahmoodify@gmail.com>
2023-03-21 01:56:47 +0500

    changing variable type size to 2 bytes

--- src/dds.net-connector-csharp.lib/EncodersAndDecoders/EncDecVariableType.cs
+++ src/dds.net-connector-csharp.lib/EncodersAndDecoders/EncDecVariableType.cs
@@ -16,9 +16,10 @@ namespace DDS.Net.Connector.EncodersAndDecoders
         public static VariableType ReadVariableType(this byte[] data, ref int offset)
         {
             Debug.Assert(data != null);
-            Debug.Assert(offset + 1 <= data.Length);
+            Debug.Assert(offset + 2 <= data.Length);
 
             int v = data[offset++];
+            v = (v << 8) | data[offset++];
 
             if (v >= 0 && v < (int)VariableType.UnknownVariableType)
             {
@@ -38,9 +39,12 @@ namespace DDS.Net.Connector.EncodersAndDecoders
         public static void WriteVariableType(this byte[] data, ref int offset, VariableType value)
         {
             Debug.Assert(data != null);
-            Debug.Assert(offset + 1 <= data.Length);
+            Debug.Assert(offset + 2 <= data.Length);
 
-            data[offset++] = (byte)value;
+            int v = (int)value;
+
+            data[offset++] = (byte)((v >> 8) & 0x0ff);
+            data[offset++] = (byte)((v >> 0) & 0x0ff);
         }
 
         /// <summary>
@@ -50,7 +54,7 @@ namespace DDS.Net.Connector.EncodersAndDecoders
         /// <returns>Number of bytes required on the buffer</returns>
         public static int GetSizeOnBuffer(this VariableType _)
         {
-            return 1;
+            return 2;
         }
     }
 }
