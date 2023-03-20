04fe9977d4179d5777819c0932c3d7ea52e315a5
usama <mahmoodify@gmail.com>
2023-03-20 17:46:22 +0500

    removing readable size check

--- src/dds.net-connector-csharp.lib/EncodersAndDecoders/SanityChecks.cs
+++ src/dds.net-connector-csharp.lib/EncodersAndDecoders/SanityChecks.cs
@@ -8,11 +8,10 @@
         /// <param name="data">The data buffer.</param>
         /// <param name="offset">Offset in the buffer.</param>
         /// <param name="requiredSize">Number of bytes required in the buffer.</param>
-        /// <param name="readableSize">Number of bytes that are readable in the buffer.</param>
         /// <returns>The same data buffer - for chaining</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        internal static byte[] ThrowIfNotHavingRequiredBytes(this byte[] data, ref int offset, int requiredSize, int readableSize)
+        internal static byte[] ThrowIfNotHavingRequiredBytes(this byte[] data, ref int offset, int requiredSize)
         {
             if (data == null)
             {
@@ -33,14 +32,6 @@
                 throw new ArgumentOutOfRangeException(
                     nameof(offset),
                     $"Array of {data.Length} bytes requires to have " +
-                    $"data with {requiredSize} bytes starting at offset {offset}");
-            }
-
-            if (endingOffset >= readableSize)
-            {
-                throw new ArgumentOutOfRangeException(
-                    nameof(offset),
-                    $"Array having readable size of {readableSize} bytes requires to have " +
                     $"data with {requiredSize} bytes starting at offset {offset}");
             }
 
